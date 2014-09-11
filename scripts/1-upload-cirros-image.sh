e26a84da786d400ac7ed019c74d2e86862e9eb94
Davide Guerri <davide.guerri@hp.com>
2014-09-11 23:57:01 +0100

    Rename scripts to give them a sequential order

---
+++ scripts/1-upload-cirros-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+. ./openstack-admin-example.rc
+
+mkdir /tmp/images
+cd /tmp/images
+
+#wget http://cdn.download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
+wget http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img
+
+glance image-create --name "cirros-0.3.3-x86_64" \
+                    --file cirros-0.3.3-x86_64-disk.img \
+                    --disk-format qcow2 \
+                    --container-format bare \
+                    --is-public True \
+                    --progress
+
+cd -
+rm -r /tmp/images
